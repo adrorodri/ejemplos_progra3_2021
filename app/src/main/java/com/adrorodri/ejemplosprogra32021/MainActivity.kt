6f02f34d6ddc25cbb83469234f9d1f797aa4170e
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2021-04-16 10:23:01 -0400

    Android views implementado

--- app/src/main/java/com/adrorodri/ejemplosprogra32021/MainActivity.kt
+++ app/src/main/java/com/adrorodri/ejemplosprogra32021/MainActivity.kt
@@ -2,10 +2,16 @@ package com.adrorodri.ejemplosprogra32021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSignIn.text = "DESDE EL CODIGO!"
+        buttonSignIn.setOnClickListener {
+            textViewForgotPassword.text = "CLICK EN BOTON!"
+        }
     }
 }
