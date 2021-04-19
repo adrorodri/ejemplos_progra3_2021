fbc67358717f32f78a54adc88de294031b0a0f1c
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2021-04-19 09:45:55 -0400

    Nuevas vistas y eventos implementados

--- app/src/main/java/com/adrorodri/ejemplosprogra32021/MainActivity.kt
+++ app/src/main/java/com/adrorodri/ejemplosprogra32021/MainActivity.kt
@@ -2,6 +2,8 @@ package com.adrorodri.ejemplosprogra32021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +11,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val validUsersList = listOf(
+            Usuario("Perrito", "12345"),
+            Usuario("Gatito", "password"),
+            Usuario("Lorito", "qwerty")
+        )
+
         buttonSignIn.text = "DESDE EL CODIGO!"
         buttonSignIn.setOnClickListener {
-            textViewForgotPassword.text = "CLICK EN BOTON!"
+            if(checkBoxRememberMe.isChecked) {
+                textViewForgotPassword.text = "CLICK EN BOTON! > REMEMBER ME ON"
+            } else {
+                textViewForgotPassword.text = "CLICK EN BOTON! > REMEMBER ME OFF"
+            }
+
+            val username = editTextTextEmailAddress.text.toString()
+            val password = editTextTextPassword.text.toString()
+
+            for(user in validUsersList) {
+                if(user.username == username && user.password == password) {
+                    textViewForgotPassword.text = "LOGIN CORRECTO!"
+                    break
+                } else {
+                    textViewForgotPassword.text = "LOGIN INCORRECTO!"
+                }
+            }
+
+            progressBar.visibility = View.VISIBLE
+        }
+
+        checkBoxRememberMe.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(checkBoxRememberMe.isChecked) {
+                textViewForgotPassword.text = "CHECKBOX CHANGED! > REMEMBER ME ON"
+            } else {
+                textViewForgotPassword.text = "CHECKBOX CHANGED! > REMEMBER ME OFF"
+            }
+        }
+
+        editTextTextEmailAddress.doAfterTextChanged { textoNuevo ->
+            textViewForgotPassword.text = "INPUT CHANGED! > " + textoNuevo
         }
     }
 }
+
+data class Usuario(val username: String, val password: String)
